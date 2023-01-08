b6781966b4e2171c43ae30d0a20c5e53fdf33d90
best800 <119391599+best800@users.noreply.github.com>
2023-01-09 00:57:57 +0530

    files updated

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const http = require("http")
+const express = require('express')
 const requestify = require('requestify');
 var port = process.env.PORT || 5500;
 const cors = require('cors')
 const app = express();
-const router = express.Router();
-
 
 app.use(cors())
 
@@ -14,21 +13,19 @@ var headers = {
     "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
 }
 
-http.createServer((req, res) => {
+app.get('/', (req, res) => {
+    requestify.request('https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY', {
+        method: 'GET',
+        headers: headers,
+        dataType: 'json'
+    }).then(function (response) {
+        res.json(JSON.stringify(response.getBody()));
+        res.end();
+    }).catch(err => {
 
-    if (req.url == '/nifty') {
-        requestify.request('https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY', {
-            method: 'GET',
-            headers: headers,
-            dataType: 'json'
-        }).then(function (response) {
-            res.end(response.getBody());
-        }).catch(err => {
-            res.end('error')
-        })
-    }
+    })
 })
-app.use('/', router)
+
 
 app.listen(port, () => {
     console.log(port)
