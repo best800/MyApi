56f3cfddc04bcff96ee864b0929a3ff63ba5557a
best800 <119391599+best800@users.noreply.github.com>
2023-01-09 01:46:33 +0530

    files updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require("http")
 const express = require('express')
 const requestify = require('requestify');
+const fs = require('fs')
 var port = process.env.PORT || 5500;
 const cors = require('cors')
 const app = express();
@@ -11,19 +12,22 @@ var headers = {
     "accept-language": "en-US,en;q=0.9",
     "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
     "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+   
 }
 
 function onRequest(req, res){
-    requestify.request('https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY', {
-        method: 'GET',
-        headers: headers,
-        dataType: 'json'
-    }).then(function (response) {
-        res.write(JSON.stringify(response.getBody()));
-        res.end();
-    }).catch(err => {
-
-    });
+   
+    res.writeHead(200,{'Content-Type':"text/html"})
+    fs.readFile('index.html','utf-8',(error,data)=>{
+        if(error){
+            res.writeHead(404)
+            res.write('Resource not found')
+        }
+        else{
+            res.write(data)
+        }
+         res.end();   
+    })
 }
 http.createServer((onRequest)).listen(port)
 
